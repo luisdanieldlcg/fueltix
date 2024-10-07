a3b4d4cf167714b62dcc6371ec2f19192f4e83e9
Luis De La Cruz <luisdanieldlcg@gmail.com>
2024-10-07 10:36:41 -0400

    Handle redirects

---
+++ client/src/app/login/page.tsx
@@ -0,0 +1,18 @@
+const Login = () => {
+  return (
+    <div className="flex w-full justify-center">
+      <div className="w-3/5">
+        <img src="fuel.png" alt="login" />
+      </div>
+      <div className="w-2/5">
+        <h1>¡Bienvenido/a! a FuelTix!</h1>
+
+        <input type="text" />
+        <input type="text" />
+
+        <button>Iniciar sesión</button>
+      </div>
+    </div>
+  );
+};
+export default Login;
