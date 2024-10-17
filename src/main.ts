65bdca81e13ae505532c53ef308081b39a9389a5
bernardobaez <bernardbaez.beno@gmail.com>
2024-10-17 08:07:50 -0400

    ..

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
 
     const app = await NestFactory.create(AppModule, { });
     app.enableCors({
-        origin: "*",
+        origin: ["http://localhost:3000"],
         credentials: true
     });
     app.setGlobalPrefix('api/v1');
